63152ee1a1480c4be5343e21d8643f6cd4b653cc
Sean Blanchfield <rashaadhassan.blanchfield@nttdata.com>
2020-12-08 11:33:16 +0100

    starter project test

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const generatePassword = require('password-generator');
 
 const app = express();
 
@@ -7,19 +8,19 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'client/build')));
 
 // Put all API endpoints under '/api'
-// app.get('/api/passwords', (req, res) => {
-//   const count = 5;
+app.get('/api/passwords', (req, res) => {
+  const count = 5;
 
-//   // Generate some passwords
-//   const passwords = Array.from(Array(count).keys()).map(i =>
-//     generatePassword(12, false)
-//   )
+  // Generate some passwords
+  const passwords = Array.from(Array(count).keys()).map((i) =>
+    generatePassword(12, false)
+  );
 
-//   // Return them as json
-//   res.json(passwords);
+  // Return them as json
+  res.json(passwords);
 
-//   console.log(`Sent ${count} passwords`);
-// });
+  console.log(`Sent ${count} passwords`);
+});
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
